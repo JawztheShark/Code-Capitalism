ab8eb2adc60f62a41af38621604de160e0c98a96
JawztheShark <>
2021-08-03 18:16:08 +0000

    added lines of code counter

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let code = 0;
 document.getElementById('increment').onclick = function (e) {
   code++;
+  let counterDisplayElem = document.querySelector('.counter-display');
+  counterDisplayElem.innerHTML = "Lines of code " + code;
   if (code == 10) {
     console.log("test");
   }
