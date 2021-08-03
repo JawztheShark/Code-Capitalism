081d6a6e840b4928f7c410a7443cc95f173554ed
JawztheShark <>
2021-08-03 19:37:53 +0000

    put displays in render and called render from tick

--- script.js
+++ script.js
@@ -1,30 +1,41 @@
-let code = 0;
 let game = {
+  code: 0,
   autocoders: 0
 };
-
+// add an autocoder
 document.getElementById('autoincrement').onclick = function (e) {
   game.autocoders += 1;
-  let counterDisplayElem = document.querySelector('.auto-display');
-  counterDisplayElem.style.display = "block";
-  counterDisplayElem.innerHTML = "Autocoders " + game.autocoders;
+};
+// add a line of code
+document.getElementById('increment').onclick = function (e) {
+  game.code += 1;
 };
 
-document.getElementById('increment').onclick = function (e) {
-  code++;
-  let counterDisplayElem = document.querySelector('.counter-display');
-  counterDisplayElem.style.display = "block";
-  counterDisplayElem.innerHTML = "Lines of code " + Math.round(code);
-  if (code >= 10) {
+// render the current game state
+function render() {
+  //adds autocoders and displays number of autocoders
+  if (game.autocoders > 0) {
+    let counterDisplayElem = document.querySelector('.auto-display');
+    counterDisplayElem.style.display = "block";
+    counterDisplayElem.innerHTML = "Autocoders " + game.autocoders;
+  }
+  if (game.code > 0) {
+    let counterDisplayElem = document.querySelector('.counter-display');
+    counterDisplayElem.style.display = "block";
+    counterDisplayElem.innerHTML = "Lines of code " + Math.round(game.code);
+  }
+  if (game.code >= 10) {
     document.getElementById("autoincrement").style.display = "block";
-    }
+  }
 };
+
+
+// exeute the main game loop
 function tick() {
-  code += game.autocoders / 20;
-  let counterDisplayElem = document.querySelector('.counter-display');
-  counterDisplayElem.style.display = "block";
-  counterDisplayElem.innerHTML = "Lines of code " + Math.round(code);
+  game.code += game.autocoders / 20;
+  render();
 }
+
 const projects = {
   hello_world: {
     name: "Hello world",
@@ -36,7 +47,11 @@ const projects = {
   },
 };
 
+// call the game loop 20 times a second
 window.setInterval(tick, 50);
+
+
+
   // , , find value, reverse list, find length of a string, modifying arrays, loops, nesting loops, recursion, match strings, reverse strings, titel case strings
   
 
