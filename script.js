11ef9ba6c61264c7350cfba957a6f1c53611445e
JawztheShark <>
2021-08-03 18:42:05 +0000

    added button that unhides at 10 clicks

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ let code = 0;
 document.getElementById('increment').onclick = function (e) {
   code++;
   let counterDisplayElem = document.querySelector('.counter-display');
+  counterDisplayElem.style.display = "block";
   counterDisplayElem.innerHTML = "Lines of code " + code;
-  if (code == 10) {
-    console.log("test");
+  if (code >= 10) {
+    document.getElementById("autoincrement").style.display = "block";
   }
   };
