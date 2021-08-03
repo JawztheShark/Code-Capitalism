711bfc31f676fc1c55ca5bbc0d022ef9fd6f2fdc
JawztheShark <>
2021-08-03 17:33:03 +0000

    added button that counts

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("test");
+let code = 0;
+document.getElementById('increment').onclick = function (e) {
+        code++;
+        if (e.preventDefault) {
+            e.preventDefault();
+        }
+        e.returnValue = false;
+       
+    };
+    console.log(code);
